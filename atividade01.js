bc522e9620ddce6c95c2ec35f5ab0ff99957d28b
Ronaldo Vieira <81587086+Ronaldo-Vieira@users.noreply.github.com>
2021-09-16 22:19:18 -0300

    upp files

---
+++ atividade01.js
@@ -0,0 +1,7 @@
+var numero = 10  
+var texto = "Olá, mundo!" 
+var verdadeiroFalso = true  
+var lista = ["Olá","mundo !"] 
+var dicionarios = {chave: "item", chave2: "item2"}  
+var indefinida = undefined 
+var nulo = null  
---
+++ atividade02.js
@@ -0,0 +1,9 @@
+var mundo = 45
+let oi = 81 
+const vida = "amo"
+
+var x = 2
+if(x >= 1) {
+    let y = 5 
+    console.log(y)
+}
